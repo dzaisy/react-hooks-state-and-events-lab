c2aad2fd82c1d3bc0d7e559fe639717162fe3acf
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-04-25 19:49:31 +0300

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,11 +5,13 @@ function Item({ name, category }) {
   function handleClick() {
     setInCart(inCart => !inCart);
   }
+
+  const colour = inCart ? "green" : "yellow";
   return (
     <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add" onClick={handleClick}>{inCart ? "Remove from Cart" : "Add to Cart"}</button>
+      <button className="add" style={{background: colour}} onClick={handleClick}>{inCart ? "Remove from Cart" : "Add to Cart"}</button>
 
     </li>
   );
